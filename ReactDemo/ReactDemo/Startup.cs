50e6d6589ca5138459a3de1e211293134e63b162
devil_coder <SHashmi@RetailMerchantServices.co.uk>
2017-01-05 16:38:14 +0000

    Initial commit

---
+++ ReactDemo/ReactDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using React;
+
+[assembly: OwinStartupAttribute(typeof(ReactDemo.Startup))]
+namespace ReactDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ReactSiteConfiguration.Configuration = new ReactSiteConfiguration()
+                .AddScript("~/Scripts/App/HelloWorld.jsx");
+            ConfigureAuth(app);
+        }
+    }
+}
